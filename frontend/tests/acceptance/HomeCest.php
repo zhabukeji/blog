89986c16549b823deadd069a1120943e51319947
zhabukeji <renzhabu@foxmail.com>
2018-07-17 14:56:00 +0800

    删掉框架原来的测试文件

--- frontend/tests/acceptance/HomeCest.php
+++ frontend/tests/acceptance/HomeCest.php
@@ -9,12 +9,9 @@ class HomeCest
     public function checkHome(AcceptanceTester $I)
     {
         $I->amOnPage(Url::toRoute('/site/index'));
-        $I->see('My Application');
-
-        $I->seeLink('About');
-        $I->click('About');
-        $I->wait(2); // wait for page to be opened
-
-        $I->see('This is the About page.');
+        $I->see('玩生活，趣科技');
+        $I->seeLink('首页');
+        $I->see('渣布');
     }
 }
+
